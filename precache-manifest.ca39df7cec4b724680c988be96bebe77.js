87ecb25e9909da3d1321c1ec95584f92382a3d41
Bruno Tomé <ibrunotome@gmail.com>
2019-09-02 00:02:04 -0300

    Updates

---
+++ precache-manifest.ca39df7cec4b724680c988be96bebe77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d53dd7a681df29beff887e1691518c2",
+    "url": "/bitcoin-salary-calculator/index.html"
+  },
+  {
+    "revision": "0eb6fbe8ecd29bda7a2e",
+    "url": "/bitcoin-salary-calculator/static/js/2.95ffd978.chunk.js"
+  },
+  {
+    "revision": "649e3f90fdc669b18a48",
+    "url": "/bitcoin-salary-calculator/static/js/main.901c1c6e.chunk.js"
+  },
+  {
+    "revision": "cb23df6c69a347e95910",
+    "url": "/bitcoin-salary-calculator/static/js/runtime~main.ed093db0.js"
+  },
+  {
+    "revision": "957f1e80ee2d9232d48b76c230da1113",
+    "url": "/bitcoin-salary-calculator/static/media/mountains.957f1e80.svg"
+  },
+  {
+    "revision": "b004c9dc7d0afaabaa165dbe5cf2834b",
+    "url": "/bitcoin-salary-calculator/static/media/stars.b004c9dc.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39fb17fc06ba5e967dc4f88ecf941c51.js"
+  "/bitcoin-salary-calculator/precache-manifest.ca39df7cec4b724680c988be96bebe77.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bitcoin-salary-calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
