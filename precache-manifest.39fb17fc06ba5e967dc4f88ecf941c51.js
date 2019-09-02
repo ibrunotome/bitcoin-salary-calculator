63bd2cc0a94d553cedd5211d2208daa2579d1d9a
Bruno Tomé <ibrunotome@gmail.com>
2019-09-01 23:59:09 -0300

    Updates

---
+++ precache-manifest.39fb17fc06ba5e967dc4f88ecf941c51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba872502690d506136a5d69cb922e26c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a0a2fcf55588a01ae70",
+    "url": "/static/js/2.cc0eac5d.chunk.js"
+  },
+  {
+    "revision": "649e3f90fdc669b18a48",
+    "url": "/static/js/main.901c1c6e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "957f1e80ee2d9232d48b76c230da1113",
+    "url": "/static/media/mountains.957f1e80.svg"
+  },
+  {
+    "revision": "b004c9dc7d0afaabaa165dbe5cf2834b",
+    "url": "/static/media/stars.b004c9dc.svg"
+  }
+]);
